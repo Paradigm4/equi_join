7a9c2402cdb7752742c049a4ceeea19e33aa53d5
Alex Poliakov <apoliakov@paradigm4.com>
2016-07-15 15:55:53 -0400

    Add tests for merge path

--- test.sh
+++ test.sh
@@ -18,47 +18,60 @@ echo "Chapter 1" >> $OUTFILE 2>&1
 iquery -aq "sort(rjoin(left, right, 'left_ids=0', 'right_ids=0'),  a,b,d)"                                                     >> $OUTFILE 2>&1
 iquery -aq "sort(rjoin(left, right, 'left_ids=0', 'right_ids=0', 'algorithm=right_to_left'),  a,b,d)"                          >> $OUTFILE 2>&1
 iquery -aq "sort(rjoin(left, right, 'left_ids=0', 'right_ids=0', 'algorithm=left_to_right'),  a,b,d)"                          >> $OUTFILE 2>&1
+iquery -aq "sort(rjoin(left, right, 'left_ids=0', 'right_ids=0', 'algorithm=merge'),          a,b,d)"                          >> $OUTFILE 2>&1
 iquery -aq "sort(rjoin(left, right, 'left_ids=0', 'right_ids=0', 'algorithm=right_to_left', 'keep_dimensions=1'),  a,b,d)"     >> $OUTFILE 2>&1
 iquery -aq "sort(rjoin(left, right, 'left_ids=0', 'right_ids=0', 'algorithm=left_to_right', 'keep_dimensions=1'),  a,b,d)"     >> $OUTFILE 2>&1
+iquery -aq "sort(rjoin(left, right, 'left_ids=0', 'right_ids=0', 'algorithm=merge',         'keep_dimensions=1'),  a,b,d)"     >> $OUTFILE 2>&1
 
 echo " " >> $OUTFILE 2>&1
 echo "Chapter 2" >> $OUTFILE 2>&1
 iquery -aq "sort(rjoin(right, left, 'left_ids=0', 'right_ids=0'),  c,d,b)"                                                     >> $OUTFILE 2>&1
 iquery -aq "sort(rjoin(right, left, 'left_ids=0', 'right_ids=0', 'algorithm=right_to_left'),  c,d,b)"                          >> $OUTFILE 2>&1
 iquery -aq "sort(rjoin(right, left, 'left_ids=0', 'right_ids=0', 'algorithm=left_to_right'),  c,d,b)"                          >> $OUTFILE 2>&1
+iquery -aq "sort(rjoin(right, left, 'left_ids=0', 'right_ids=0', 'algorithm=merge'),          c,d,b)"                          >> $OUTFILE 2>&1
 iquery -aq "sort(rjoin(right, left, 'left_ids=0', 'right_ids=0', 'algorithm=right_to_left', 'keep_dimensions=1'),  c,d,b)"     >> $OUTFILE 2>&1
 iquery -aq "sort(rjoin(right, left, 'left_ids=0', 'right_ids=0', 'algorithm=left_to_right', 'keep_dimensions=1'),  c,d,b)"     >> $OUTFILE 2>&1
+iquery -aq "sort(rjoin(right, left, 'left_ids=0', 'right_ids=0', 'algorithm=merge',         'keep_dimensions=1'),  c,d,b)"     >> $OUTFILE 2>&1
 
 echo " " >> $OUTFILE 2>&1
 echo "Chapter 3" >> $OUTFILE 2>&1
 iquery -aq "rjoin(left, right, 'left_ids=0,~0', 'right_ids=0,~0')"                                                             >> $OUTFILE 2>&1
 iquery -aq "rjoin(left, right, 'left_ids=0,~0', 'right_ids=0,~0', 'algorithm=right_to_left')"                                  >> $OUTFILE 2>&1
 iquery -aq "rjoin(left, right, 'left_ids=0,~0', 'right_ids=0,~0', 'algorithm=left_to_right')"                                  >> $OUTFILE 2>&1
+iquery -aq "rjoin(left, right, 'left_ids=0,~0', 'right_ids=0,~0', 'algorithm=merge')"                                          >> $OUTFILE 2>&1
 iquery -aq "rjoin(left, right, 'left_ids=0,~0', 'right_ids=0,~0', 'algorithm=right_to_left', 'keep_dimensions=T')"             >> $OUTFILE 2>&1
 iquery -aq "rjoin(left, right, 'left_ids=0,~0', 'right_ids=0,~0', 'algorithm=left_to_right', 'keep_dimensions=T')"             >> $OUTFILE 2>&1
+iquery -aq "rjoin(left, right, 'left_ids=0,~0', 'right_ids=0,~0', 'algorithm=merge',         'keep_dimensions=T')"             >> $OUTFILE 2>&1
 
 echo " " >> $OUTFILE 2>&1
 echo "Chapter 4" >> $OUTFILE 2>&1
 iquery -aq "rjoin(right, left, 'left_ids=~0,0', 'right_ids=~0,0')"                                                             >> $OUTFILE 2>&1
 iquery -aq "rjoin(right, left, 'left_ids=~0,0', 'right_ids=~0,0', 'algorithm=right_to_left', 'keep_dimensions=0')"             >> $OUTFILE 2>&1
 iquery -aq "rjoin(right, left, 'left_ids=~0,0', 'right_ids=~0,0', 'algorithm=left_to_right', 'keep_dimensions=0')"             >> $OUTFILE 2>&1
+iquery -aq "rjoin(right, left, 'left_ids=~0,0', 'right_ids=~0,0', 'algorithm=merge',         'keep_dimensions=0')"             >> $OUTFILE 2>&1
 iquery -aq "rjoin(right, left, 'left_ids=~0,0', 'right_ids=~0,0', 'algorithm=right_to_left', 'keep_dimensions=true')"          >> $OUTFILE 2>&1
 iquery -aq "rjoin(right, left, 'left_ids=~0,0', 'right_ids=~0,0', 'algorithm=left_to_right', 'keep_dimensions=true')"          >> $OUTFILE 2>&1
+iquery -aq "rjoin(right, left, 'left_ids=~0,0', 'right_ids=~0,0', 'algorithm=merge',         'keep_dimensions=true')"          >> $OUTFILE 2>&1
 
 echo " " >> $OUTFILE 2>&1
 echo "Chapter 5" >> $OUTFILE 2>&1
 iquery -aq "sort(rjoin(left, right, 'left_ids=~0', 'right_ids=1'), i,a,b,c)"                                                   >> $OUTFILE 2>&1
 iquery -aq "sort(rjoin(left, right, 'left_ids=~0', 'right_ids=1', 'algorithm=left_to_right', 'keep_dimensions=F'), i,a,b,c)"   >> $OUTFILE 2>&1 
 iquery -aq "sort(rjoin(left, right, 'left_ids=~0', 'right_ids=1', 'algorithm=right_to_left', 'keep_dimensions=F'), i,a,b,c)"   >> $OUTFILE 2>&1
+iquery -aq "sort(rjoin(left, right, 'left_ids=~0', 'right_ids=1', 'algorithm=merge',        'keep_dimensions=F'), i,a,b,c)"    >> $OUTFILE 2>&1
 iquery -aq "sort(rjoin(left, right, 'left_ids=~0', 'right_ids=1', 'algorithm=left_to_right', 'keep_dimensions=TRUE'), i,a,b,c)">> $OUTFILE 2>&1
 iquery -aq "sort(rjoin(left, right, 'left_ids=~0', 'right_ids=1', 'algorithm=right_to_left', 'keep_dimensions=TRUE'), i,a,b,c)">> $OUTFILE 2>&1
+iquery -aq "sort(rjoin(left, right, 'left_ids=~0', 'right_ids=1', 'algorithm=merge', 'keep_dimensions=TRUE'), i,a,b,c)"        >> $OUTFILE 2>&1
 
 echo " " >> $OUTFILE 2>&1
 echo "Chapter 6" >> $OUTFILE 2>&1
 iquery -aq "sort(rjoin(right, left, 'left_ids=1', 'right_ids=~0'), d,c,a,b)"                                                    >> $OUTFILE 2>&1
 iquery -aq "sort(rjoin(right, left, 'left_ids=1', 'right_ids=~0', 'algorithm=left_to_right', 'keep_dimensions=FALSE'), d,c,a,b)">> $OUTFILE 2>&1
 iquery -aq "sort(rjoin(right, left, 'left_ids=1', 'right_ids=~0', 'algorithm=right_to_left', 'keep_dimensions=false'), d,c,a,b)">> $OUTFILE 2>&1
+iquery -aq "sort(rjoin(right, left, 'left_ids=1', 'right_ids=~0', 'algorithm=merge',         'keep_dimensions=false'), d,c,a,b)">> $OUTFILE 2>&1
 iquery -aq "sort(rjoin(right, left, 'left_ids=1', 'right_ids=~0', 'algorithm=left_to_right', 'keep_dimensions=true'), d,c,a,b)" >> $OUTFILE 2>&1
 iquery -aq "sort(rjoin(right, left, 'left_ids=1', 'right_ids=~0', 'algorithm=right_to_left', 'keep_dimensions=TRUE'), d,c,a,b)" >> $OUTFILE 2>&1
+iquery -aq "sort(rjoin(right, left, 'left_ids=1', 'right_ids=~0', 'algorithm=merge', 'keep_dimensions=TRUE'), d,c,a,b)" >> $OUTFILE 2>&1
 
 diff test.out test.expected
+
